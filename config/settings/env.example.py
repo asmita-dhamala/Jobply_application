1e02a00879ee3f273dcb2a2878ffba3c70b32a3e
asmita-dhamala <asmitadhamala540@gmail.com>
2024-05-29 23:22:48 -0700

    new

---
+++ config/settings/env.example.py
@@ -0,0 +1,14 @@
+import os
+from .base import *
+DEBUG = True
+ALLOWED_HOSTS = []
+SECRET_KEY = ''
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media/')
+
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+DATABASES = {}
